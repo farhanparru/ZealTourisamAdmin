323eaf5ba1add764764dc07bab61cf86ebca467c
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-06-12 14:58:29 -0500

    Mobile view aligenment adjust again re

--- src/AdminUI/Dashboard.jsx
+++ src/AdminUI/Dashboard.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { FaCalendarAlt, FaUsers, FaMoneyBillWave, FaMosque, FaUmbrellaBeach } from 'react-icons/fa';
 import { BiSolidPlaneAlt } from 'react-icons/bi';
